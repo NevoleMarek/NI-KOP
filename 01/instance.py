ba260760a03e2c8d73c4ce894a6a10a44366b18c
NevoleMarek <nevole.marek@gmail.com>
2021-09-22 19:19:12 +0200

    Add Instace class of knapsack

---
+++ 01/instance.py
@@ -0,0 +1,18 @@
+class Bag:
+    def __init__(self):
+        self.items = []
+        self.capacity = 0
+        self.id = 0
+        self.min_price = 0
+
+    @classmethod
+    def from_line(cls, line):
+        bag = cls()
+        input_split = line.split()
+        bag.id = input_split[0]
+        bag.capacity = input_split[2]
+        bag.min_price = input_split[3]
+        for i in range(len(input_split[4:])//2):
+            item = (input_split[2 * i + 4], input_split[2* i + 5])
+            bag.items.append(item)
+        return bag
