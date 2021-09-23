cd9fe3e5d2c15e92119378ffd935027147e0fbd2
NevoleMarek <nevole.marek@gmail.com>
2021-09-23 15:47:30 +0200

    Change: Output benchmark stats to file

--- 01/run.py
+++ 01/run.py
@@ -11,11 +11,16 @@ method = {
 
 def main():
     args = Parser().parse()
-    with open(args.file) as file:
-        for line in file:
+    sol = args.file.replace('inst', 'sol').replace('R', 'K')
+    res = args.file.replace('tests', 'results')
+    with open(args.file) as in_file, open(res, 'w') as res_file:
+        for line in in_file:
             instance = Bag.from_line(line)
-            #solver = method[args.method](instance)
-            #solver.solve()
+            solver = method[args.method](instance)
+            solver.solve()
+            stats = solver.stats()
+            res_file.write(f'{instance.id} {stats["configs_tried"]}\n')
+
 
 if __name__ == '__main__':
     main()
