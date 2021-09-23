b5d58c3d38f5266fe1886a2bbe7c8fde59ff48ea
NevoleMarek <nevole.marek@gmail.com>
2021-09-23 17:12:04 +0200

    Change: Output file written in csv format

--- 01/run.py
+++ 01/run.py
@@ -11,15 +11,15 @@ method = {
 
 def main():
     args = Parser().parse()
-    sol = args.file.replace('inst', 'sol').replace('R', 'K')
-    res = args.file.replace('tests', 'results')
+    res = args.file.replace('tests', 'results').replace('inst', args.method)
     with open(args.file) as in_file, open(res, 'w') as res_file:
+        res_file.write('id,tries\n')
         for line in in_file:
             instance = Bag.from_line(line)
             solver = method[args.method](instance)
             solver.solve()
             stats = solver.stats()
-            res_file.write(f'{instance.id} {stats["configs_tried"]}\n')
+            res_file.write(f'{instance.id},{stats["configs_tried"]}\n')
 
 
 if __name__ == '__main__':
