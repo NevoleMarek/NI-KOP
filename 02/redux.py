1f80c91fd7db5cf06f7cb90cf234a7909e0e9cb4
Marek Nevole <nevole.marek@gmail.com>
2021-10-01 15:26:12 +0200

    02 Add Greedy and Redux heuristics

---
+++ 02/redux.py
@@ -0,0 +1,50 @@
+class Redux:
+    def __init__(self, instance):
+        self.instance = instance
+        self._stats = {
+            'solved' : False,
+            'best_cost': 0,
+            'best_weight': 0,
+            'solution': [0 for i in range(len(instance.items))]
+
+        }
+
+    def solve(self):
+        if self._stats['solved']:
+            print('Already solved')
+            return
+        orig_pos, sorted_items = zip(
+                                *sorted(
+                                    enumerate(self.instance.items),
+                                    key=lambda x: x[1][0]/x[1][1]
+                                    )
+                                )
+        self._stats['solved'] = True
+        self.__redux(sorted_items, orig_pos)
+
+        # Find the single best item to fit in bag and compare with greedy
+        # heuristic result. Return the best ot the two.
+        for item_id, item in enumerate(sorted_items):
+            if item[0] <= self.instance.capacity:
+                if item[1] > self._stats['best_cost']:
+                    self._stats['best_cost'] = item[1]
+                    self._stats['best_weight'] = item[0]
+                    self._stats['solution'] = [0 for i in range(len(instance.items))]
+                    self._stats['solution'][orig_pos[item_id]] = 1
+                break
+
+
+    def stats(self):
+        return self._stats
+
+    def __redux(self, sorted_items, orig_pos):
+        weight, cost = 0, 0
+
+        for item_id, item in enumerate(sorted_items):
+            if weight + item[0] <= self.instance.capacity:
+                self._stats['solution'][orig_pos[item_id]] = 1
+                weight += item[0]
+                cost += item[1]
+
+        self._stats['best_cost'] = cost
+        self._stats['best_weight'] = weight
